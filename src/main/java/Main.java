752fdcf3cfa6db44c850e98e4d2cbab7e2b927e7
erick2507 <169068468+erick2507@users.noreply.github.com>
2024-09-02 12:28:46 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+    System.out.println(" Login");
+ String login = sc.next();
+int senhaAdmin = 12345;
+
+
+
+
+
+    
+   sc.close();
+  }
+if ( Login == "admin" ) {
+ System.out.println(" coloque sua senha ");
+
+  int senha = sc.nextInt();
+  if(Senha == senhaAdmin){
+System.out.println("acesso permitido");
+  } else {
+    System.out.println ("Acesso negado ");
+  }
+
+  System.out.println("login invalido");
+    
+
+
+
+
